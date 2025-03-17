1abc98320e421c5f0147cc2957fc928117cc5dc0
Tomas Gonzalez <tsgonzalez01@gmail.com>
2025-03-17 02:40:02 -0400

    Initial Commit

---
+++ Connections-App/Connections_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Connections_AppApp.swift
+//  Connections-App
+//
+//  Created by Tomas Gonzalez on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Connections_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Connections-AppTests/Connections_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Connections_AppTests.swift
+//  Connections-AppTests
+//
+//  Created by Tomas Gonzalez on 3/17/25.
+//
+
+import Testing
+@testable import Connections_App
+
+struct Connections_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
